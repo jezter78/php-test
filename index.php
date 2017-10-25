6dbbec351db6306129f79e0ffe349e26d22297fd
jezter78 <33082211+jezter78@users.noreply.github.com>
2017-10-25 16:07:06 +0800

    remove comment

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <?php 
     require 'testhead.php';
     ?>
     <body>
         <?php
-         echo 'hello'
+         echo 'hello world'
         ?>
     </body>
 </html>
